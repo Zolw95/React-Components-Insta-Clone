f540788c7fde7aa1d25cad2efdca2b6b9499264d
unknown <miloszz87@gmail.com>
2020-04-27 00:07:24 -0400

    homework

--- src/components/PostsContainer/PostsPage.js
+++ src/components/PostsContainer/PostsPage.js
@@ -1,15 +1,26 @@
 //Complete the necessary code in this file
 // import useState
-import React from "react";
+import React, {useState} from "react";
 import Post from "./Post";
 import "./Posts.css";
+import dummyData from "../../dummy-data";
 // import data 
+//console.log(dummyData);
 
-const PostsPage = () => {
+const PostsPage = (props) => {
   // set up state for your data
+  const [data] = useState(dummyData);
+  //console.log(data);
+
+
+
   return (
     <div className="posts-container-wrapper">
-      {/* map through data here to return a Post and pass data as props to Post */}
+      {/* map through data here to return a Post and pass data as props to Post */
+        data.map((post)=>{
+          return <Post post={post} />
+        })
+      }
     </div>
   );
 };
